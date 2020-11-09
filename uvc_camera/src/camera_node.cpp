5c1bc577e68368e5e38f5f895e89de355780c2ed
vtretyakov <slava@tretyakov.de>
2020-11-09 16:48:00 +0100

    make both nodehandles private

--- uvc_camera/src/camera_node.cpp
+++ uvc_camera/src/camera_node.cpp
@@ -6,7 +6,7 @@
 int main (int argc, char **argv) {
   ros::init(argc, argv, "uvc_camera");
 
-  uvc_camera::Camera camera(ros::NodeHandle(), ros::NodeHandle("~"));
+  uvc_camera::Camera camera(ros::NodeHandle("~"), ros::NodeHandle("~"));
 
   ros::spin();
   return 0;
